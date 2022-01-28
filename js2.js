29795581ebe33afe272397036499aaa8fc79746f
Astha x) <95849912+Asttttha@users.noreply.github.com>
2022-01-28 13:12:13 +0530

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,15 @@
+const email= prompt('Enter your email');
+const passw=prompt('Enter your password');
+// if(email= 'dexy2020@gmail.com'){
+//     alert('Hurray, you have logged in :D');}
+// else{
+//     alert('Try again :(');
+// }
+if (email=='dexy2020@gmail.com'){
+        alert('Hurray, you have logged in :D')
+    }
+else{
+        alert('Try again :(')
+    }
+    
+    
