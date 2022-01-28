16aabbf46ca463c59e641ba01351089877551580
Astha x) <95849912+Asttttha@users.noreply.github.com>
2022-01-28 13:13:54 +0530

    day2 assignment

--- js2.js
+++ js2.js
@@ -1,5 +1,5 @@
-const email= prompt('Enter your email');
-const passw=prompt('Enter your password');
+const email= prompt('Enter your Email');
+const passw=prompt('Enter your Password');
 // if(email= 'dexy2020@gmail.com'){
 //     alert('Hurray, you have logged in :D');}
 // else{
